dd61b11397659802e6c3108f85cd1a705e3383a5
TKACZ <tkaczu19@gmail.com>
2021-09-25 10:44:32 +0200

    chore: init project

---
+++ src/test/java/pl/academy/pokemonacademyapi/PokemonAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.academy.pokemonacademyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAcademyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
